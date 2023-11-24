823c3a0338bcd7c44e387cc2762c32ec7d87689f
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-11-24 23:17:25 +0530

    added

--- Java Refresher/Class - 2/even_odd.java
+++ Java Refresher/Class - 2/even_odd.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class even_odd {
-    
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        int num = scn.nextInt();
+
+        if (num % 2 == 0)
+            System.out.println("Even");
+
+        else
+            System.out.println("Odd");
+
+        scn.close();
+    }
 }
