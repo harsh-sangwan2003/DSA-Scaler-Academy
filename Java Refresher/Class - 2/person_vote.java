c511ec10b27d1b2f643845af7b30b428aa4c712f
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-11-24 21:40:55 +0530

    added

---
+++ Java Refresher/Class - 2/person_vote.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+      Scanner scn = new Scanner(System.in);
+      int age = scn.nextInt();
+
+      if(age>=18)
+      System.out.println("Person can vote");
+
+      else
+      System.out.println("Person cannot vote");
+  }
+}
