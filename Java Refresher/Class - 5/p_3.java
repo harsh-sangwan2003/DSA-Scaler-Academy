1833d8ff91037e181146091213f59bc829e39794
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-12-01 21:41:48 +0530

    added

--- Java Refresher/Class - 5/p_3.java
+++ Java Refresher/Class - 5/p_3.java
@@ -6,10 +6,11 @@ public class p_3 {
 
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        int m = scn.nextInt();
 
         for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= n; j++)
+            for (int j = 1; j <= m; j++)
                 System.out.print("*");
 
             System.out.println();
