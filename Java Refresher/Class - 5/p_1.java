fae8b7eb74bbc5bc7adbec7e4db3133a10aaeb33
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-12-01 21:20:19 +0530

    added

---
+++ Java Refresher/Class - 5/p_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p_1 {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for (int i = 1; i <= n; i++)
+            System.out.print("*");
+
+        scn.close();
+    }
+}
