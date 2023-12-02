90d756e18a3e9f95b61c64549284270a0c1ced62
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-12-02 10:01:53 +0530

    added

---
+++ Java Refresher/Class - 5/p_9.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class p_9 {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int nsp = n - 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print("*");
+
+            for (int csp = 1; csp <= nsp; csp++)
+                System.out.print(" ");
+
+            System.out.println("*");
+
+            nsp--;
+        }
+
+        scn.close();
+    }
+}
