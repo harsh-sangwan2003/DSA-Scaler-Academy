581f3e8735b12db8d6a929465dda6eb6959f40d4
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-20 11:59:27 +0530

    added

---
+++ Intermediate/Class - 10/majority_element.java
@@ -0,0 +1,27 @@
+class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int majorityElement(final int[] A) {
+
+        int ele = A[0], count = 1;
+
+        for(int i=1; i<A.length; i++){
+
+            if(count==0){
+
+                ele = A[i];
+                count++;
+            }
+
+            else{
+
+                if(A[i]==ele)
+                count++;
+
+                else
+                count--;
+            }
+        }
+
+        return ele;
+    }
+}
