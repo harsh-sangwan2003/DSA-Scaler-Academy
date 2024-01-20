bf840e43434c33be6f18040f9151999a3dc804f4
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-20 12:33:10 +0530

    added

---
+++ Intermediate/Class - 10/majority_element2.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int repeatedNumber(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        int n = nums.length;
+        if (count1 > n / 3) {
+            return candidate1;
+        } else if (count2 > n / 3) {
+            return candidate2;
+        } else {
+            return -1;
+        }
+    }
+}
