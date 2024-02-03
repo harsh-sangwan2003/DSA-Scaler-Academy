d93957aea0b5df14ff7262af3a18b8cacabcd051
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-02-03 12:27:30 +0530

    added

--- Advanced/DSA 1/Class 3/first_missing_integer.java
+++ Advanced/DSA 1/Class 3/first_missing_integer.java
@@ -1,38 +1,38 @@
 public class first_missing_integer {
     public int firstMissingPositive(int[] A) {
 
-        int N = A.length+2;
+        int N = A.length + 2;
         boolean flag = false;
 
-        for(int i=0; i<A.length; i++){
+        for (int i = 0; i < A.length; i++) {
 
-            if(A[i]<=0)
-            A[i] = N+2;
+            if (A[i] <= 0)
+                A[i] = N + 2;
 
-            if(A[i]==1)
-            flag = true;
+            if (A[i] == 1)
+                flag = true;
         }
 
-        if(!flag)
-        return 1;
+        if (!flag)
+            return 1;
 
-        for(int i=0; i<A.length; i++){
+        for (int i = 0; i < A.length; i++) {
 
-            int idx = Math.abs(A[i])-1;
+            int idx = Math.abs(A[i]) - 1;
 
-            if(idx>=0 && idx<A.length){
+            if (idx >= 0 && idx < A.length) {
 
-                if(A[idx]>0)
-                A[idx]*=-1;
+                if (A[idx] > 0)
+                    A[idx] *= -1;
             }
-        } 
-
-        for(int i=0; i<A.length; i++){
-
-            if(A[i]>0)
-            return i+1;
         }
 
-        return A.length+1;
+        for (int i = 0; i < A.length; i++) {
+
+            if (A[i] > 0)
+                return i + 1;
+        }
+
+        return A.length + 1;
     }
 }
---
+++ Advanced/DSA 1/Class 3/merge_overlapping_intervals.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+// Definition for an interval.
+class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
+
+public class merge_overlapping_intervals {
+    public Interval[] merge(Interval[] intervals) {
+
+        if (intervals.length <= 1)
+            return intervals;
+
+        Arrays.sort(intervals, (i1, i2) -> i1.start - i2.start);
+
+        ArrayList<Interval> res = new ArrayList<>();
+        Interval current = intervals[0];
+        res.add(current);
+
+        for (Interval interval : intervals) {
+
+            int current_st = current.start, current_et = current.end;
+            int next_st = interval.start, next_et = interval.end;
+
+            if (next_st <= current_et)
+                current.end = Math.max(current_et, next_et);
+
+            else {
+
+                current = interval;
+                res.add(current);
+            }
+        }
+
+        Interval[] arr = new Interval[res.size()];
+        int idx = 0;
+
+        for (Interval val : res) {
+
+            arr[idx++] = val;
+        }
+
+        return arr;
+    }
+}
