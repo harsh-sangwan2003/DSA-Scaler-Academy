30fd37f8be9b0ffa082b69345cb80bd7593c8094
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-02-09 19:44:33 +0530

    added

---
+++ Advanced/DSA 1/Class 5/subarray_or.java
@@ -0,0 +1,22 @@
+public class subarray_or {
+    public int solve(int[] A) {
+
+        int n = A.length;
+        int[] idx = new int[32];
+        long ans = 0;
+        for (int i = 1; i <= n; ++i) {
+            long tmp = A[i - 1];
+            for (int j = 0; j <= 31; ++j) {
+                long pw = 1 << j;
+                if ((tmp & pw) != 0) { // if jth bit is set
+                    ans += pw * i; // add its contribution in ans for all subarrays ending at index i
+                    idx[j] = i; // store the index for next elements
+                } else if (idx[j] != 0) // if jth bit is not set
+                {
+                    ans += pw * idx[j]; // add its contribution in ans for all subarrays ending at index i using
+                } // the information of last element having jth bit set
+            }
+        }
+        return (int) (ans % 1000000007);
+    }
+}
