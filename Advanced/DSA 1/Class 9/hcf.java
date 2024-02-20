50cba23a887a7a0a0ba1e1f7ae8596c0ad038bd9
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-02-20 12:02:16 +0530

    added

--- Advanced/DSA 1/Class 9/hcf.java
+++ Advanced/DSA 1/Class 9/hcf.java
@@ -1,9 +1,9 @@
-public class hcd {
+public class hcf {
     public int gcd(int A, int B) {
 
-        if(B==0)
-        return A;
+        if (B == 0)
+            return A;
 
-        return gcd(B,A%B);
+        return gcd(B, A % B);
     }
 }
